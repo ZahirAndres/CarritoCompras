84f5dc2fb6b6564cf3fdcb8424ac34c342ce5c3d
brandon-p3 <brandongustavo350@gmail.com>
2025-03-28 18:29:39 -0600

    Cambios

--- MotoPronto/src/app/app.routes.ts
+++ MotoPronto/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { VerProductosComponent } from './components/productos/ver-productos/ver-productos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: VerProductosComponent, pathMatch: 'full' }
+];
